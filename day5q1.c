d776690fdbd41f6694e5df293264455870f78a38
hritikShrivastava931 <hritikshrivastava931@gmail.com>
2026-02-05 23:43:17 +0530

    Add files via upload

---
+++ day5q1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main()
+{
+    int p, q;
+    int i = 0, j = 0, k = 0;
+
+    scanf("%d", &p);
+
+    int a[p];
+    for (i = 0; i < p; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    scanf("%d", &q);
+
+    int b[q];
+    for (i = 0; i < q; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+
+    int c[p + q];
+
+    i = 0;
+    j = 0;
+
+    while (i < p && j < q)
+    {
+        if (a[i] <= b[j])
+        {
+            c[k] = a[i];
+            i++;
+        }
+        else
+        {
+            c[k] = b[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < p)
+    {
+        c[k] = a[i];
+        i++;
+        k++;
+    }
+
+    while (j < q)
+    {
+        c[k] = b[j];
+        j++;
+        k++;
+    }
+
+    for (i = 0; i < p + q; i++)
+    {
+        printf("%d ", c[i]);
+    }
+
+    return 0;
+}
