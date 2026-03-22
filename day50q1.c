5ea65fe1d4250131cd1e6348aa4fef8e50b1f931
hritikShrivastava931 <hritikshrivastava931@gmail.com>
2026-03-22 23:32:17 +0530

    Add files via upload

---
+++ day50q1.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int value)
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int value)
+{
+    if (root == NULL)
+    {
+        return createNode(value);
+    }
+
+    if (value < root->data)
+    {
+        root->left = insert(root->left, value);
+    }
+    else if (value > root->data)
+    {
+        root->right = insert(root->right, value);
+    }
+
+    return root;
+}
+
+int search(struct Node* root, int key)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    if (root->data == key)
+    {
+        return 1;
+    }
+
+    if (key < root->data)
+    {
+        return search(root->left, key);
+    }
+    else
+    {
+        return search(root->right, key);
+    }
+}
+
+int main()
+{
+    struct Node* root = NULL;
+
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 70);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    int key;
+    printf("Enter value to search: ");
+    scanf("%d", &key);
+
+    if (search(root, key))
+    {
+        printf("Found\n");
+    }
+    else
+    {
+        printf("Not Found\n");
+    }
+
+    return 0;
+}
