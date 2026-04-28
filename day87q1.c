85554f38c5b1b2fd4934572a151c1701935c2b27
hritikShrivastava931 <hritikshrivastava931@gmail.com>
2026-04-28 23:23:04 +0530

    Add files via upload

---
+++ day87q1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int key)
+{
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+
+        if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    int key;
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        printf("\nElement found at index %d", result);
+    else
+        printf("\nElement not found");
+
+    return 0;
+}
